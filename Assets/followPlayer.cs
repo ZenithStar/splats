ede08902e19a674615aa36cceaf313012daa7f8c
stanleycherrygit <stonley1980@gmail.com>
2023-06-18 19:59:46 +0200

    Summary
    Added an enemy object,made a script for following the player. Which I need to correct, it doesnt work. Added basic HUD which doesnt work yet.

---
+++ Assets/followPlayer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followPlayer : MonoBehaviour
+{
+    private Transform target;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();    
+            }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed*Time.deltaTime);
+    }
+}
